940aa603401382e1956283867d3d6adc160b01b7
Danny Aiquipa <danny@mapbox.com>
2015-09-25 12:59:10 -0500

    update

--- main.js
+++ main.js
@@ -76,6 +76,23 @@ $(function() {
   	}
 
 
+function getAlbumPhotos(){
+            FB.api('/me/albums',  function(resp) {
+                //Log.info('Albums', resp);
+                var ul = document.getElementById('albums');
+                for (var i=0, l=resp.data.length; i<l; i++){
+                    var
+                        album = resp.data[i],
+                        li = document.createElement('li'),
+                        a = document.createElement('a');
+                    a.innerHTML = album.name;
+                    a.href = album.link;
+                    li.appendChild(a);
+                    ul.appendChild(li);
+                }
+            });
+        };
+
 
   	$(document).on('click', '#login', function(e) {
   		e.preventDefault();
