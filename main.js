7d0c4376a46b94a40a737aa29f08dd2b30878561
Danny Aiquipa <danny@mapbox.com>
2015-09-25 15:23:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	var app_id = '1682310898660786';
+	var app_id = '386184524909608';
 	var scopes = 'email, user_friends';
 
 	var btn_login = '<a href="#" id="login" class="btn btn-primary">Iniciar sesión</a>';
